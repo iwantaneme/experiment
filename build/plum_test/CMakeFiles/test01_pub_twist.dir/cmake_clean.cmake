e8f8230d4f3006dff603e8ba98cdce176f2bcd60
iwantaname <784874860@qq.com>
2021-08-17 20:04:41 +0800

    加入服务测试

---
+++ build/plum_test/CMakeFiles/test01_pub_twist.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/home/iwantaname/experiment/devel/lib/plum_test/test01_pub_twist"
+  "/home/iwantaname/experiment/devel/lib/plum_test/test01_pub_twist.pdb"
+  "CMakeFiles/test01_pub_twist.dir/src/test01_pub_twist.cpp.o"
+  "CMakeFiles/test01_pub_twist.dir/src/test01_pub_twist.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test01_pub_twist.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/plum_test/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/iwantaname/experiment/src/plum_test
+# Build directory: /home/iwantaname/experiment/build/plum_test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
